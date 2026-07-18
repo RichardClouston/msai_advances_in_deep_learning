91dbb0eb27cde8a4703d25f8a2e59e91c9ae7e8b
Richard Clouston <cloustonr@aol.com>
2026-07-18 10:54:44 -0500

    Implement batch generation

--- homework3/homework/base_llm.py
+++ homework3/homework/base_llm.py
@@ -112,7 +112,28 @@ class BaseLLM:
                 for r in self.batched_generate(prompts[idx : idx + micro_batch_size], num_return_sequences, temperature)
             ]
 
-        raise NotImplementedError()
+        self.tokenizer.padding_side = "left"
+        inputs = self.tokenizer(prompts, padding=True, return_tensors="pt").to(self.device)
+
+        outputs = self.model.generate(
+            **inputs,
+            max_new_tokens=50,
+            do_sample=temperature > 0,
+            temperature=temperature if temperature > 0 else None,
+            num_return_sequences=num_return_sequences or 1,
+            eos_token_id=self.tokenizer.eos_token_id,
+        )
+
+        generated_tokens = outputs[:, inputs["input_ids"].shape[1]:]
+        decoded = self.tokenizer.batch_decode(generated_tokens, skip_special_tokens=True)
+
+        if num_return_sequences is None:
+            return decoded
+
+        return [
+            decoded[i * num_return_sequences:(i + 1) * num_return_sequences]
+            for i in range(len(prompts))
+        ]
 
     def answer(self, *questions) -> list[float]:
         """
